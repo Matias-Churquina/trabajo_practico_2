10e4a12ec2548706dcc3217c5c1a310760025982
Matias <Matias@DESKTOP-9B903QQ>
2024-04-25 11:24:21 -0300

    Punto N°2 terminado.

--- tp2_ChurquinaMatias/src/ar/edu/unju/fi/ejercicio2/model/Efemeride.java
+++ tp2_ChurquinaMatias/src/ar/edu/unju/fi/ejercicio2/model/Efemeride.java
@@ -8,5 +8,46 @@ public class Efemeride {
 	private Integer dia;
 	private String detalle;
 	
+	public Efemeride() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	public String toString() {
+		return "Efemeride [codigo=" + codigo + ", mes=" + mes + ", dia=" + dia + ", detalle=" + detalle + "]";
+	}
+
+	public Integer getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Integer codigo) {
+		this.codigo = codigo;
+	}
+
+	public Mes getMes() {
+		return mes;
+	}
+
+	public void setMes(Mes mes) {
+		this.mes = mes;
+	}
+
+	public Integer getDia() {
+		return dia;
+	}
+
+	public void setDia(Integer dia) {
+		this.dia = dia;
+	}
+
+	public String getDetalle() {
+		return detalle;
+	}
+
+	public void setDetalle(String detalle) {
+		this.detalle = detalle;
+	}
+	
 	
 }
