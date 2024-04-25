517a114117866a66b0b2d97c3eb277f1a0344a04
Matias <Matias@DESKTOP-9B903QQ>
2024-04-25 00:31:25 -0300

    Punto 1 terminado.

--- tp2_ChurquinaMatias/src/ar/edu/unju/fi/ejercicio1/main/Main.java
+++ tp2_ChurquinaMatias/src/ar/edu/unju/fi/ejercicio1/main/Main.java
@@ -15,6 +15,7 @@ public class Main {
 		sc = new Scanner(System.in);
 		int op = 0;
 		do {
+			System.out.println();
 			System.out.println("========= PRODUCTOS ========");
 			System.out.println("1 - Crear un producto.");
 			System.out.println("2 - Mostar productos.");
@@ -42,8 +43,12 @@ public class Main {
 		    }
 		
 		}while( op != 4 );
-
 	}
+	/**
+	 * Propósito: Este módulo muestra un menú por consola para que el
+	 * usuario pueda elegir un pais de acuerdo a su número asignado.
+	 * 
+	 */
 	public static int opPais() {
 		int op = 0;
 		System.out.println("    Elija su origen de fabricación ");
@@ -57,6 +62,11 @@ public class Main {
 		op = sc.nextInt();
 		return op;
 	}
+	/**
+	 * Propósito: Este módulo muestra un menú por consola para que el
+	 * usuario pueda elegir una categoria de acuerdo a su número asignado.
+	 * 
+	 */
 	public static int opCategoria() {
 		int op = 0;
 		System.out.println("    Elija su categoría =====");
@@ -70,7 +80,12 @@ public class Main {
 		op = sc.nextInt();
 		return op;
 	}
-	
+	/**
+	 * Propósito: Este módulo crea un nuevo objeto Producto solicitando
+	 * por consola al usuario los datos del producto.
+	 * También agrega un objeto Producto a un array 'productos'.
+	 * 
+	 */
 	public static void crearProducto() {
 		if(productos == null) {
 			productos = new ArrayList<>();
@@ -81,31 +96,47 @@ public class Main {
 		p.setCodigo(sc.nextInt());
 		sc.nextLine();
 		System.out.print("Ingrese descripción: ");
-		p.setDescripcion(sc.nextLine());	
-		try {
-			System.out.print("Ingrese precio unitario: ");
-			p.setPrecioUnit(sc.nextDouble());
-		}catch(Exception e) {
-			System.out.println("Debe usar coma ',' como separador decimal.");
+		p.setDescripcion(sc.nextLine());
+		while(true) {
+			try {
+				System.out.print("Ingrese precio unitario: ");
+				p.setPrecioUnit(sc.nextDouble());
+				break;
+			}catch(Exception e) {
+				System.out.println("Debe usar coma ',' como separador decimal.");
+				sc.nextLine();
+			}
 		}
+		while(true) {
 		try {
 			int o = opPais();
 			OrigenFab[] valPais = OrigenFab.values();
 			OrigenFab pais = valPais[o-1];
 			p.setOrigenFab(pais);
+			break;
 		}catch(Exception e) {
 				System.out.println("ERROR. Debe ingresar una opción de acuerdo al país.");
 		}
+		}
+		while(true) {
 		try {
 		 	int c = opCategoria();
 		    Categoria[] valCat = Categoria.values();
 		    Categoria categ = valCat[c-1];
 		    p.setCategoria(categ);
+		    break;
 		}catch(Exception e) {
 				System.out.println("ERROR. Debe ingresar una opción de acuerdo a la categoria.");
 		}
+		}
 	    productos.add(p);
+	    System.out.println("Producto agregado correctamente.");
 	}
+	/**
+	 * Propósito: Este módulo muestra de forma iterativa los datos 
+	 * de cada objeto producto agragado a el array productos.
+	 * 
+	 */
 	public static void mostrarProductos() {
 		if(productos != null) {
 			System.out.println("===== Lista de Productos =====");
@@ -114,6 +145,13 @@ public class Main {
 			System.out.println("ERROR. La lista está vacía.");
 		}
 	}
+	/**
+	 * Propósito: El siguiente múdulo función busca un objeto producto
+	 * por su código, este solicitado al usuario por consola.
+	 * Si lo encuentra devuelve  el objeto producto de otra forma
+	 * solo devuelve null.
+	 * 
+	 */
 	public static Producto  buscarProducto(int codigo) {
 		Producto Encontrado = null;
 		for(Producto cod: productos) {
@@ -124,6 +162,13 @@ public class Main {
 		}
 		return Encontrado;
 	}
+	/**
+	 * Propósito: Este módulo modifica determinados datos de un producto.
+	 * Para esto solicita al usuario el código del producto para buscarlo
+	 * y si lo encuentra solicita al usuario los nuevos datos, de otra
+	 * manera informa al usuario que el código es incorrecto.
+	 * 
+	 */
 	public static void modificarProd() {
 		System.out.println("===== Modificar Producto =====");
 		System.out.print("Ingrese código del producto a modificar: ");	
@@ -133,25 +178,41 @@ public class Main {
 			sc.nextLine();
 			System.out.print("Ingrese nueva descripción: ");
 			prod.setDescripcion(sc.nextLine());
-			System.out.print("Ingrese nuevo precio unitario: ");
-			prod.setPrecioUnit(sc.nextDouble());
-			try {
-			int o = opPais();
-			OrigenFab[] valPais = OrigenFab.values();
-			prod.setOrigenFab(valPais[o-1]);
-			}catch(Exception e) {
-				System.out.println("ERROR. Debe ingresar una opción de acuerdo al país.");
+			while(true) {
+				try {
+					System.out.print("Ingrese nuevo precio unitario: ");
+					prod.setPrecioUnit(sc.nextDouble());
+					break;
+				}catch(Exception e) {
+					System.out.println("Debe usar coma ',' como separador decimal.");
+					sc.nextLine();
+				}
 			}
-			try {
-			int c = opCategoria();
-		    Categoria[] valCat = Categoria.values();
-		    prod.setCategoria(valCat[c-1]);
-			}catch(Exception e) {
-				System.out.println("ERROR. Debe ingresar una opción de acuerdo a la categoria.");
+			while(true) {
+				try {
+					int o = opPais();
+					OrigenFab[] valPais = OrigenFab.values();
+					prod.setOrigenFab(valPais[o-1]);
+					break;
+				}catch(Exception e) {
+					System.out.println("ERROR. Debe ingresar una opción de acuerdo al país.");
+					sc.next();
+				}
 			}
-		} else {
+			while(true) {
+				try {
+					int c = opCategoria();
+				    Categoria[] valCat = Categoria.values();
+				    prod.setCategoria(valCat[c-1]);
+				    System.out.println("Prodcuto modificado correctamente.");
+				    break;
+			}catch(Exception e) {
+					System.out.println("ERROR. Debe ingresar una opción de acuerdo a la categoria.");
+					sc.next();
+			}
+			}
+		}else {
 			System.out.println("ERROR. El código ingresado no existe.");
-			
 		}
 	}
 }
